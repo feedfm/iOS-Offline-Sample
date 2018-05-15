3c7b5fb185cb8f3b92025862ef385070d24adc17
Arveen <arveen@feed.fm>
2018-05-15 13:28:20 -0700

    Initial Commit

---
+++ SampleOfflineApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleOfflineApp
+//
+//  Created by Arveen kumar on 5/15/18.
+//  Copyright © 2018 Feed FM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleOfflineApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SampleOfflineApp
+//
+//  Created by Arveen kumar on 5/15/18.
+//  Copyright © 2018 Feed FM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
